9818337781b056ba816a03f402a4aea7ac7a9ab4
ZionHJS <hjszion@gmail.com>
2020-12-02 13:42:10 -0800

    first commit

---
+++ src/main/java/com/galaxy/demo/GalaxychatbotsApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxychatbotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxychatbotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galaxy/demo/GalaxychatbotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxychatbotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
