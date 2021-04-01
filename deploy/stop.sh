461f07291607534e711eb3b7848636e2f831452b
zionhjs <hjszion@gmail.com>
2021-04-01 11:47:00 -0700

    why failed

--- deploy/stop.sh
+++ deploy/stop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#Service_name=galaxy-chatbot
-#Docker_running=`sudo docker ps |sudo grep $Service_name|wc -l`
-#
-#if [ -n "$Docker_running" ];then
-#     sudo docker rm -f $Service_name >/dev/null 2>1
-#     sudo docker rm -f `sudo docker ps -qa` >/dev/null 2>1
-#     sudo docker rmi -f `sudo docker images|sudo grep -v openjdk|sudo awk '{print $3}'|sudo grep -v IMAGE` >/dev/null 2>1
-#else
-#     sudo echo 'exit 0'
-#fi
+Service_name=galaxy-chatbot
+Docker_running=`sudo docker ps |sudo grep $Service_name|wc -l`
+
+if [ -n "$Docker_running" ];then
+     sudo docker rm -f $Service_name >/dev/null 2>1
+     sudo docker rm -f `sudo docker ps -qa` >/dev/null 2>1
+     sudo docker rmi -f `sudo docker images|sudo grep -v openjdk|sudo awk '{print $3}'|sudo grep -v IMAGE` >/dev/null 2>1
+else
+     sudo echo 'exit 0'
+fi
 
 
